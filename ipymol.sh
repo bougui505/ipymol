77c37c49bfd2a9ef4d1676484ceb159510a2f3aa
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-02-27 11:01:03 +0100

    Initial commit 	new file:   ipymol.sh 	new file:   ipython_script.py

---
+++ ipymol.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-02-27 09:03:23 (UTC+0100)
+
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+ipython3 -i "$DIRSCRIPT/ipython_script.py"
---
+++ ipython_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-02-27 09:00:55 (UTC+0100)
+
+from IPython import get_ipython
+import pymol
+from pymol import cmd
+import numpy
+
+ipython = get_ipython()
+ipython.magic("load_ext autoreload")
+ipython.magic("autoreload 2")
+
+pymol.finish_launching()
