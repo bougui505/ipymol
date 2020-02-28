0b42fac9155161de06a588e4173d058506576758
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-02-28 13:37:29 +0100

    Use a specific profile file (and therefore history) specific to pymol 	modified:   ipymol.sh

--- ipymol.sh
+++ ipymol.sh
@@ -6,4 +6,4 @@
 # 2020-02-27 09:03:23 (UTC+0100)
 
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
-ipython3 -i "$DIRSCRIPT/ipython_script.py"
+ipython --profile=pymol -i "$DIRSCRIPT/ipython_script.py"
